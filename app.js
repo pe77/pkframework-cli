e317685e928a94c1253761108b930b6b0fc1c5f3
P <p321654987@outlook.com>
2017-03-23 16:00:58 -0300

    pk init v0.1 ok

--- app.js
+++ app.js
@@ -4,14 +4,18 @@
 var commander 	= require('commander');
 var chalk 		= require('chalk');
 var livereload 	= require('livereload');
-// var connect  	= require('connect');
 var http 		= require("http");
+var https 		= require("https");
 var serveStatic = require('serve-static')
 var finalhandler = require('finalhandler')
 var ts 			= require('typescript');
 var exec 		= require('child_process').exec;
-var fs = require('fs')
+var fs 			= require('fs-extra')
+// var ghdownload 	= require('github-download');
+var AdmZip 		= require('adm-zip');
+var ProgressBar = require('progress');
 
+// general vars
 var lrserver;
 var server;
 var serverStatic;
@@ -22,16 +26,72 @@ var indexPath = process.cwd() + '/index.html';
 
 
 commander
-.version('0.0.1')
-.arguments('go')
-// .option('-u, --username <username>', 'The user to authenticate as')
-.action(function(command, dir) {
+.version('0.3.1')
+.arguments('arg')
+// .option('-u, --username <username>', 'github username')
+.action(function(command) {
 
 	if(command == 'go')
 		go();
 	//
+
+	if(command == 'init')
+		init();
+	//
+
 })
 .parse(process.argv);
+
+// init a new workspace
+function init()
+{
+	console.log(chalk.green('init project ...'));
+
+	var zipFile = process.cwd() + "/pkframework-starter-master.zip";
+	var zipFolder = process.cwd() + "/pkframework-starter-master";
+
+	// check if dir is empty
+	console.log(chalk.yellow('[1/3]') + chalk.green(' check folder'));
+	fs.readdir(process.cwd(), function(err, files) {
+	    if (err) {
+	       console.log(chalk.red('err...'), err);
+	    } else {
+	       if (!files.length || true) {
+
+	       		console.log(chalk.yellow('[2/3]') + chalk.green(' download file'));
+
+	       		// dowload file
+				// https://github.com/pe77/pkframework-starter/archive/master.zip
+				// https://nodeload.github.com/pe77/pkframework-starter/zip/master
+				download('https://nodeload.github.com/pe77/pkframework-starter/zip/master', zipFile, err=>{
+					if (err) return console.log(chalk.red('err...'), err);
+
+					console.log(chalk.yellow('[3/3]') + chalk.green(' extract'));
+
+					// extract	
+					var zip = new AdmZip(zipFile);
+					zip.extractAllTo(process.cwd(), true);
+
+					// copy and delete zip and zip-folder
+					fs.copy(zipFolder, process.cwd(), err => {
+					  if (err) return console.error(err)
+					  
+
+					  	// remove dir and zip
+						fs.remove(zipFolder);
+						fs.unlinkSync(zipFile);
+
+						console.log(chalk.cyan('YEAH BABY!'));
+						console.log(chalk.gray('try: ') + chalk.green.bold('pkframe go'));
+					});
+				});
+
+	       }else{
+	       		console.log('dir ['+chalk.gray(process.cwd())+'] is not ' + chalk.red('empty'));
+	       }
+	    }
+	});
+}
 
 // start server
 function go()
@@ -149,3 +209,37 @@ function go()
 		require("openurl").open(serverUrl);
 	})
 }
+
+
+
+function download(url, dest, cb) {
+  
+
+  var file = fs.createWriteStream(dest);
+  var request = https.get(url, function(response) {
+
+  	
+  	var len = parseInt(response.headers['content-length'], 10);
+
+  	var bar = new ProgressBar(chalk.green('downloading') + chalk.gray('[:bar]') +' :percent :etas', {
+	    complete: '=',
+	    incomplete: ' ',
+	    width: 20,
+	    total: len
+	  });
+
+  	response.on('data', (chunk)=> {
+	    bar.tick(chunk.length);
+	});
+
+
+
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
