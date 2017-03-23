b4b5bd23a44004a4ccb79ccd12ad1586a28ba5ac
P <p321654987@outlook.com>
2017-03-22 22:20:44 -0300

    for home dbg

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-program
-.arguments('<file>')
-.option('-u, --username <username>', 'The user to authenticate as')
-.option('-p, --password <password>', 'The user\'s password')
-.action(function(file) {
-  console.log(
-  	'user: %s pass: %s file: %s', 
-  	program.username, 
-  	program.password, 
-  	file);
+// load shit
+var commander 	= require('commander');
+var chalk 		= require('chalk');
+var livereload 	= require('livereload');
+// var connect  	= require('connect');
+var http 		= require("http");
+var serveStatic = require('serve-static')
+var finalhandler = require('finalhandler')
+var ts 			= require('typescript');
+
+var lrserver;
+var server;
+var serverStatic;
+var data;
+var port = 8011;
+var serverUrl;
+
+
+commander
+.version('0.0.1')
+.arguments('go')
+// .option('-u, --username <username>', 'The user to authenticate as')
+.action(function(command, dir) {
+
+	if(command == 'go')
+		go();
+	//
 })
 .parse(process.argv);
+
+// start server
+function go()
+{
+	// init liverload
+	lrserver = livereload.createServer();
+
+	// simple static server
+	serverStatic = serveStatic(process.cwd(), {'index': ['index.html', 'index.htm']})
+	var server = http.createServer(function(request, response) {
+	  serverStatic(request, response, finalhandler(request, response));
+	});
+
+	server.listen(port);
+
+	// whatch dirs
+	var whatchDir = process.cwd() + '/dist';
+	whatchDirs = [
+		process.cwd() + '/dist',
+		process.cwd() + '/index.html'
+	];
+	lrserver.watch(whatchDirs[0], whatchDirs[1]);
+
+	serverUrl = 'http://localhost:' + port;
+
+	console.log('Runnin on: ' + chalk.green(serverUrl));
+
+	// process tps
+	require('ts-node').register({
+		'project':process.cwd()
+	});
+
+	// open browser
+	// require("openurl").open(serverUrl);
+}
