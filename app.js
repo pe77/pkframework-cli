53c2cf2d08a6cb3e4f98c9909672d52949f57f0f
P <p321654987@outlook.com>
2017-03-26 10:23:20 -0300

    upd readme

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var indexPath = process.cwd() + '/index.html';
 
 
 commander
-.version('0.8.0')
+.version('0.8.5')
 .arguments('arg')
 // .option('-u, --username <username>', 'github username')
 .option('-p, --package <package>', 'select package to start [default=basic]')
